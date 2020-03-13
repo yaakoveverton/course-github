b20210f03abbe528322b7d04f5fa8d8b989d2441
Thiago Everton <86.everton@gmail.com>
2020-03-13 19:18:14 -0300

    mensagem de boa noite

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -5,7 +5,8 @@ public class Program {
 	public static void main (String args[]) {
 		
 		System.out.println("Good evening");
-		
+		System.out.println("Good  night");
+		System.out.println("Good morning");
  	}
 
 }
